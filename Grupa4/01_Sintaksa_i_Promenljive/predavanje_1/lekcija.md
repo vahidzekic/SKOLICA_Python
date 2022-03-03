5cf8b5dbdefa0b2d0e89e47e00a8d52d2a6f7685
Vahid Zekic <vahidzekic@gmail.com>
2022-03-03 13:07:16 +0100

    feat: Grupa 4 - Sintaksa i Promenljive - Predavanje 1

---
+++ Grupa4/01_Sintaksa_i_Promenljive/predavanje_1/lekcija.md
@@ -0,0 +1,32 @@
+# Predavanje 1 — Zdravo Python: Instalacija i Prvo Pokretanje
+
+## 1.1 Zašto Python?
+
+Python se koristi u:
+- **Web razvoju** (Django, Flask)
+- **Data Science** (Pandas, NumPy)
+- **Mašinsko učenje** (TensorFlow, PyTorch)
+- **Automatizacija** (skripting, DevOps)
+
+## 1.2 Pokretanje
+
+```bash
+python3 --version
+python3 moj_program.py
+```
+
+## 1.3 print()
+
+```python
+print("Zdravo!")
+print("A", "B", sep="-")   # A-B
+print("Tekst", end=" ")    # Bez novog reda
+```
+
+## 1.4 Komentari
+- `#` za jednolinijske
+- `\"\"\"...\"\"\"` za višelinijske (docstring)
+
+## Rezime
+- Python je interpretirani, dinamički tipiziran jezik
+- `print()` za ispis na konzolu
---
+++ Grupa4/01_Sintaksa_i_Promenljive/predavanje_1/primer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# ŠKOLICA Python — Grupa4 / 01 / Predavanje 1
+# Tema: Prvo pokretanje, print()
+
+# --- Prvi program ---
+print("Zdravo iz ŠKOLICA Python — Grupa4!")
+print("Danas je naš prvi čas programiranja.")
+print("")
+
+# --- print() sa raznim tipovima ---
+print("=== print() demonstracija ===")
+print("Tekst: Ovo je string")
+print("Ceo broj:", 100)
+print("Decimalni:", 2.718)
+print("Logička vrednost:", False)
+print("")
+
+# --- Separator i end ---
+print("Sa separatorom:", "A", "B", "C", sep=" -> ")
+print("Sa zarezom:", "X", "Y", "Z", sep=", ")
+print("Prva polovina...", end=" ")
+print("Druga polovina!")
+print("")
+
+# --- Matematika u print ---
+print("=== Matematika ===")
+print("2 + 3 =", 2 + 3)
+print("10 * 5 =", 10 * 5)
+print("2 ** 8 =", 2 ** 8)
+print("17 % 5 =", 17 % 5)
