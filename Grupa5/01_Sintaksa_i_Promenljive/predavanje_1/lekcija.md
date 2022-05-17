251c5b8dce2f0b9d2e95ce676ac1ce4909247c0c
Vahid Zekic <vahidzekic@gmail.com>
2022-05-17 13:52:43 +0100

    feat: Grupa 5 - Sintaksa i Promenljive - Predavanje 1

---
+++ Grupa5/01_Sintaksa_i_Promenljive/predavanje_1/lekcija.md
@@ -0,0 +1,27 @@
+# Predavanje 1 — Python: Početak Putovanja
+
+## 1.1 Šta je Python?
+- Kreirao **Guido van Rossum**, 1991.
+- Interpretirani, multi-paradigmatski jezik
+- Koristi se: web, AI, data science, automatizacija
+
+## 1.2 Pokretanje
+```bash
+python3 --version
+python3 program.py
+```
+
+## 1.3 print()
+```python
+print("Tekst")
+print("A", "B", sep="-")
+print("Bez novog reda", end=" ")
+```
+
+## 1.4 Komentari
+- `#` jednolinijski
+- `\"\"\"...\"\"\"` višelinijski
+
+## Rezime
+- Python: čista sintaksa, indentacija za blokove
+- `print()` — osnovna funkcija za ispis
---
+++ Grupa5/01_Sintaksa_i_Promenljive/predavanje_1/primer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# ŠKOLICA Python — Grupa5 / 01 / Predavanje 1
+# Tema: Prvi program, print()
+
+print("Dobrodošli u ŠKOLICA Python — Grupa5!")
+print("Python verzija: 3.x")
+print("")
+
+print("=== print() varijacije ===")
+print("String:", "Ovo je tekst")
+print("Broj:", 2025)
+print("Decimal:", 9.81)
+print("Bool:", True)
+print("")
+
+print("Separator:", "X", "Y", "Z", sep=" | ")
+print("Bez novog reda:", end=" ")
+print("nastavak na istoj liniji!")
+print("")
+
+print("=== Matematika ===")
+print("5 + 3 =", 5 + 3)
+print("2 ** 10 =", 2 ** 10)
+print("100 // 7 =", 100 // 7)
+print("100 % 7 =", 100 % 7)
