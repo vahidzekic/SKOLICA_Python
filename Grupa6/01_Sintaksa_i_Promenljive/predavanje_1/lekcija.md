de25dd59049deb77ee535da8dc03bc52b01f3f21
Vahid Zekic <vahidzekic@gmail.com>
2022-08-03 13:09:05 +0100

    feat: Grupa 6 - Sintaksa i Promenljive - Predavanje 1

---
+++ Grupa6/01_Sintaksa_i_Promenljive/predavanje_1/lekcija.md
@@ -0,0 +1,29 @@
+# Predavanje 1 — Uvod u Python
+
+## 1.1 Python u praksi
+- Web: Django, Flask, FastAPI
+- AI/ML: TensorFlow, PyTorch
+- Automatizacija i skripting
+- Naučno računanje: NumPy, SciPy
+
+## 1.2 Pokretanje
+```bash
+python3 --version
+python3 program.py
+```
+
+## 1.3 print()
+```python
+print("Tekst")
+print("A", "B", sep=" | ")
+print("Isto", end=" ")
+print("linija")
+```
+
+## 1.4 Komentari
+- `#` jednolinijski
+- Trostruki navodnici za višelinijske
+
+## Rezime
+- Python: čitljiv, dinamički tipiziran
+- print() je osnovna I/O funkcija
---
+++ Grupa6/01_Sintaksa_i_Promenljive/predavanje_1/primer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# ŠKOLICA Python — Grupa6 / 01 / Predavanje 1
+# Tema: print(), komentari
+
+print("Dobrodošli u ŠKOLICA Python — Grupa6!")
+print("Tema ove grupe: Muzika i Knjige")
+print("")
+
+print("=== print() varijacije ===")
+print("Pesma:", "Bohemian Rhapsody")
+print("Godina:", 1975)
+print("Trajanje:", 5.55)
+print("Klasik:", True)
+print("")
+
+print("Sep:", "Do", "Re", "Mi", "Fa", "Sol", sep=" - ")
+print("Nastavak", end="... ")
+print("iste linije!")
+print("")
+
+print("=== Računanje ===")
+print("7 + 5 =", 7 + 5)
+print("3 ** 4 =", 3 ** 4)
+print("99 // 10 =", 99 // 10)
+print("99 % 10 =", 99 % 10)
