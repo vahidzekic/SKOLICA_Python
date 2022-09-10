924eef4d81a286a625e17306948067ef131e4c33
Vahid Zekic <vahidzekic@gmail.com>
2022-09-10 21:52:35 +0100

    feat: Grupa 6 - Obrada Grešaka (Try/Except) - Predavanje 1

---
+++ Grupa6/07_Obrada_Gresaka_Try_Except/predavanje_1/lekcija.md
@@ -0,0 +1,7 @@
+# Predavanje 1 â try/except/else/finally
+
+## Äesti izuzeci i struktura
+
+## Rezime
+- Hvatati specifiÄne izuzetke
+- finally uvek izvrÅ¡ava
---
+++ Grupa6/07_Obrada_Gresaka_Try_Except/predavanje_1/primer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Grupa6 / 07 / P1 â try/except
+
+try: print(10 / 0)
+except ZeroDivisionError: print("Deljenje nulom!")
+
+try: int("abc")
+except ValueError as e: print(f"ValueError: {e}")
+
+try: [1,2][9]
+except IndexError: print("Index van opsega!")
+
+try:
+    r = 50 / 2
+except ZeroDivisionError:
+    print("GreÅ¡ka!")
+else:
+    print(f"OK: {r}")
+finally:
+    print("Finally.")
