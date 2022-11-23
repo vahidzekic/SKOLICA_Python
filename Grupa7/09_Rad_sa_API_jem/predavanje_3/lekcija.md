aa63cdb513c09ae06227d79abbc5da6d1a3363ab
Vahid Zekic <vahidzekic@gmail.com>
2022-11-23 03:16:12 +0100

    feat: Grupa 7 - Rad sa API-jem - Predavanje 3

---
+++ Grupa7/09_Rad_sa_API_jem/predavanje_3/lekcija.md
@@ -0,0 +1,6 @@
+# Predavanje 3 â Session, Timeout
+
+## Session(), timeout, error handling
+
+## Rezime
+- Uvek timeout
---
+++ Grupa7/09_Rad_sa_API_jem/predavanje_3/primer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Grupa7 / 09 / P3 â Session, timeout
+try:
+    import requests
+except ImportError: print("pip install requests"); exit(1)
+
+BASE = "https://jsonplaceholder.typicode.com"
+
+print("=== Session ===")
+with requests.Session() as s:
+    s.headers.update({"Accept": "application/json"})
+    r1 = s.get(f"{BASE}/posts/1")
+    r2 = s.get(f"{BASE}/posts/2")
+    print(f"  1: {r1.json()['title'][:35]}...")
+    print(f"  2: {r2.json()['title'][:35]}...")
+
+print("\n=== Timeout ===")
+try:
+    r = requests.get("https://httpbin.org/delay/1", timeout=3)
+    print(f"  OK: {r.status_code}")
+except requests.Timeout: print("  Timeout!")
+except requests.ConnectionError: print("  No connection!")
+
+print("\n=== Query ===")
+r = requests.get(f"{BASE}/comments", params={"postId": 1})
+if r.ok: print(f"  Comments: {len(r.json())}")
